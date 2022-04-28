c74013149aad543b9bb88aa28c7f75e812e6d646
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-28 17:23:18 +0300

    DemoQa extends from TestBase class has been added

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -1,27 +1,16 @@
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static com.codeborne.selenide.Selenide.open;
 
-public class DemoQaTests {
-    @BeforeAll
-    public static void openPage() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        open("https://demoqa.com/automation-practice-form");
-    }
+public class DemoQaTests extends TestBase{
 
     @Test
     @DisplayName("Тест на проверку заполнения формы регистрации")
     void checkFormFilled() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-
         RegistrationFormPage registrationFormPage = new RegistrationFormPage(); //не нужно создавать метод main,
         // т.к. аннотация @Test позволяет создавать объекты другого класса внутри себя.
         executeJavaScript("$('footer').remove()");// Отключает футер.
