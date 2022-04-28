c95727118d09a18e64a805ca873afebaee573dad
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-28 17:22:13 +0300

    TestBase class has been added

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import helpers.Attachments;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void openPage() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        open("https://demoqa.com/automation-practice-form");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+    }
+}
