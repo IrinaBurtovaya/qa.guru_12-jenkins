c79ca011b4eceb45c5e500e6cf9b6f26ee47fdd1
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-28 12:55:12 +0300

    all files have been restructured

---
+++ src/test/java/CalendarComponent.java
@@ -0,0 +1,10 @@
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CalendarComponent {
+    public void setDate(int monthIndex, String yearIndex, String day) {
+        $x("//select[@class='react-datepicker__month-select']").selectOption(monthIndex);
+        $x("//select[@class='react-datepicker__year-select']").selectOptionByValue(yearIndex);
+        $x("//div[@class='react-datepicker__day react-datepicker__day--0" + day +
+                " react-datepicker__day--weekend']").click();
+    }
+}
---
+++ src/test/java/DemoQaTests.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DemoQaTests {
+    @BeforeAll
+    public static void openPage() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browserSize = "1920x1080";
+        open("automation-practice-form");
+    }
+
+    @Test
+    void checkFormFilled() {
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(); //не нужно создавать метод main,
+        // т.к. аннотация @Test позволяет создавать объекты другого класса внутри себя.
+
+        registrationFormPage.setFirstName()
+                .setLastName()
+                .setEmail()
+                .setGender("Male")
+                .setPhoneNumber()
+                .setDateOfBirth(7, "1990", "05")
+                .setSubject("Maths")
+                .setHobbies("Music")
+                .uploadFile()
+                .setStateAndCity()
+                .checkResults();
+    }
+}
