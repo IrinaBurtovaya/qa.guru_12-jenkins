d27a3ba58bd97304bc4d41aac36a7845334b59f6
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-28 17:36:46 +0300

    work with video

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -1,5 +1,3 @@
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +8,7 @@ public class DemoQaTests extends TestBase{
     @Test
     @DisplayName("Тест на проверку заполнения формы регистрации")
     void checkFormFilled() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
+
         RegistrationFormPage registrationFormPage = new RegistrationFormPage(); //не нужно создавать метод main,
         // т.к. аннотация @Test позволяет создавать объекты другого класса внутри себя.
         executeJavaScript("$('footer').remove()");// Отключает футер.
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attachments;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,6 +12,8 @@ public class TestBase {
 
     @BeforeAll
     public static void openPage() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
