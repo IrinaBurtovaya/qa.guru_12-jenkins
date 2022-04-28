e8ac26d966c30f7f7ad2e9eea7170cff885baf0b
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-28 16:31:16 +0300

    listener has been added

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
@@ -15,7 +18,10 @@ public class DemoQaTests {
     }
 
     @Test
+    @DisplayName("Тест на проверку заполнения формы регистрации")
     void checkFormFilled() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         RegistrationFormPage registrationFormPage = new RegistrationFormPage(); //не нужно создавать метод main,
         // т.к. аннотация @Test позволяет создавать объекты другого класса внутри себя.
         executeJavaScript("$('footer').remove()");// Отключает футер.
