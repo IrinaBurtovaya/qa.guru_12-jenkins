b372c429156d954aebb96f2636f21e1929cd8bcf
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-05-03 20:31:58 +0300

    lambda steps have been added

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -2,8 +2,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static io.qameta.allure.Allure.step;
 
-public class DemoQaTests extends TestBase{
+public class DemoQaTests extends TestBase {
 
     @Test
     @DisplayName("Тест на проверку заполнения формы регистрации")
@@ -14,16 +15,21 @@ public class DemoQaTests extends TestBase{
         executeJavaScript("$('footer').remove()");// Отключает футер.
         executeJavaScript("$('#fixedban').remove()");// Отключает рекламу.
 
-        registrationFormPage.setFirstName()
-                .setLastName()
-                .setEmail()
-                .setGender("Male")
-                .setPhoneNumber()
-                .setDateOfBirth(7, "1990", "05")
-                .setSubject("Maths")
-                .setHobbies("Music")
-                .uploadFile()
-                .setStateAndCity()
-                .checkResults();
+        step("Заполняем форму регистрации", () -> {
+            registrationFormPage.setFirstName()
+                    .setLastName()
+                    .setEmail()
+                    .setGender("Male")
+                    .setPhoneNumber()
+                    .setDateOfBirth(7, "1990", "05")
+                    .setSubject("Maths")
+                    .setHobbies("Music")
+                    .uploadFile()
+                    .setStateAndCity();
+        });
+
+        step("Проверяем результаты теста", () -> {
+            registrationFormPage.checkResults();
+        });
     }
 }
