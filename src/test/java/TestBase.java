99e02092598d533f386985ebdf7b0f76a3e50760
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-05-05 17:50:07 +0300

    working with owner

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,21 +1,23 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attachments;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     public static void openPage() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        Configuration.remote = "https://" + config.login() + ":" + config.password() + "@" + config.hub();
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
