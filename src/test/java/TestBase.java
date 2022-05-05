9728b8e660ee698c3c098c8d047f5ad13a83245b
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-05-05 22:57:43 +0300

    parametr has been changed

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
-    static String selenoidUrl = System.getProperty("selenoid", "selenoid.autotests.cloud/wd/hub");
+    static String selenoidUrl = System.getProperty("selenoidUrl", "selenoid.autotests.cloud/wd/hub");
 
     @BeforeAll
     public static void openPage() {
